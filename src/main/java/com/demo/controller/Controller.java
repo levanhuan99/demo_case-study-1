c7d9f31e42ff3bd4c6155e692e1be16d83839cf1
levanhuan99 <61698248+levanhuan99@users.noreply.github.com>
2020-07-07 17:19:32 +0700

    ok

--- src/main/java/com/demo/controller/Controller.java
+++ src/main/java/com/demo/controller/Controller.java
@@ -2,5 +2,7 @@ package com.demo.controller;
 public class Controller {
     public static void main(String[] args) {
         System.out.println("test3");
+
+        System.out.println("hello");
     }
 }
