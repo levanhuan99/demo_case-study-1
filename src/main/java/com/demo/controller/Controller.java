3caec4dd2c27ea6845e8dc0f86a0f4c99f41dbf4
hieu <hieutran2811@gmail.com>
2020-07-07 16:47:28 +0700

    test 333

--- src/main/java/com/demo/controller/Controller.java
+++ src/main/java/com/demo/controller/Controller.java
@@ -1,7 +1,7 @@
 package com.demo.controller;
 public class Controller {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test3");
     }
 
 }
