329bddd0994fe13e094921756faa33306b96ac51
daoxuandong1997 <daoxuandong1997@gmail.con>
2020-07-07 16:52:04 +0700

    ditmemay

--- src/main/java/com/demo/controller/Controller.java
+++ src/main/java/com/demo/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.demo.controller;
 public class Controller {
     public static void main(String[] args) {
-        System.out.println("test2");
+        System.out.println("test1");
     }
+
 }
